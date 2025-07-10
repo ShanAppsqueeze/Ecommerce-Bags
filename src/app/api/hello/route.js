d57b1d20f35931d66f263cc78f2bbd0c09a97a38
ShanAppsqueeze <shan.javed@appsqueeze.com>
2025-07-10 17:38:14 +0500

    some feature update- 2

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -1,32 +1,32 @@
 
 // app/api/hello/route.js
 
-// export async function GET(request) {
-//   const data = {
-//     message: "Hello from Next.js API!",
-//     time: new Date().toISOString(),
-//   };
+export async function GET(request) {
+  const data = {
+    message: "Hello from Next.js API!",
+    time: new Date().toISOString(),
+  };
 
-//   return Response.json(data);
-// }
-
-
-
-import connectMongo from "@/lib/mongodb";
-import Contact from "@/models/contactusModel/User";
-
-export async function POST(request) {
-  try {
-    await connectMongo();
-    const body = await request.json();
-    const { name, email, message } = body;
-
-    const newContact = await Contact.create({ name, email, message });
-    return Response.json({ success: true, data: newContact });
-  } catch (error) {
-    return Response.json({ success: false, error: "Failed to save contact" }, { status: 500 });
-  }
+  return Response.json(data);
 }
+
+
+
+// import connectMongo from "@/lib/mongodb";
+// import Contact from "@/models/contactusModel/User";
+
+// export async function POST(request) {
+//   try {
+//     await connectMongo();
+//     const body = await request.json();
+//     const { name, email, message } = body;
+
+//     const newContact = await Contact.create({ name, email, message });
+//     return Response.json({ success: true, data: newContact });
+//   } catch (error) {
+//     return Response.json({ success: false, error: "Failed to save contact" }, { status: 500 });
+//   }
+// }
 
 
 
